1b3a3ac827d82e9330ea39a45e52dda44560fdac
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-01-20 09:04:28 +0000

    Many Fixes.

--- src/Entity/UserManagement/User.php
+++ src/Entity/UserManagement/User.php
@@ -5,37 +5,9 @@ use VS\UsersBundle\Model\User as BaseUser;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="IAUM_Users")
+ * @ORM\Table(name="VSUM_Users")
  */
 class User extends BaseUser
 {
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="integer")
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    protected $id;
-    
-    /**
-     * @ORM\OneToOne(targetEntity="App\Entity\UserManagement\UserInfo", cascade={"persist"})
-     * @ORM\JoinColumn(name="user_info_id", referencedColumnName="id")
-     */
-    protected $userInfo;
-    
-    public function getUserInfo()
-    {
-        return $this->userInfo;
-    }
-    
-    public function setUserInfo( $userInfo )
-    {
-        $this->userInfo = $userInfo;
-        
-        return $this;
-    }
-    
-    public function __construct()
-    {
-        parent::__construct();
-    }
+   
 }
--- src/Entity/UserManagement/UserInfo.php
+++ src/Entity/UserManagement/UserInfo.php
@@ -5,15 +5,9 @@ use VS\UsersBundle\Model\UserInfo as BaseUserInfo;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="IAUM_UsersInfo")
+ * @ORM\Table(name="VSUM_UsersInfo")
  */
 class UserInfo extends BaseUserInfo
 {
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="integer")
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    protected $id;
     
 }
