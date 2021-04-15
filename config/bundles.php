f0439aa33a594f1e6ef3ff69be9d2109f3b2a3b6
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-04-15 05:15:08 +0000

    Using User Profile Pictures.

--- config/bundles.php
+++ config/bundles.php
@@ -28,4 +28,6 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
